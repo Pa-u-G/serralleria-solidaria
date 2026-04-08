d1932157cdbd014cdeb53ea6903eb9e9d631959a
Pa-u-G <pau.garbajosa@inslapineda.cat>
2026-04-08 12:54:22 +0200

    arreglar rutas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,24 +26,24 @@ function App() {
 
         <Routes>
 
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/create" element={<Create_product />} />
-          <Route path="/products/edit/:id" element={<Edit_product />} />
+          <Route path="/admin/" element={<Dashboard />} />
+          <Route path="/admin/categories" element={<Categories />} />
+          <Route path="/admin/products" element={<Products />} />
+          <Route path="/admin/products/create" element={<Create_product />} />
+          <Route path="/admin/products/edit/:id" element={<Edit_product />} />
 
-          <Route path="/caracteristicas" element={<Caracteristicas />} />
+          <Route path="/admin/caracteristicas" element={<Caracteristicas />} />
 
-          <Route path="/caracteristicas/:id" element={<CharacteristicTypeShow />} />
+          <Route path="/admin/caracteristicas/:id" element={<CharacteristicTypeShow />} />
 
-          <Route path="/packs" element={<Packs />} />
+          <Route path="/admin/packs" element={<Packs />} />
 
-          <Route path="/packs/:id" element={<PackShow />} />
-          <Route path="/packs/create" element={<Pack_create />} />
-          <Route path="/packs/edit/:id" element={<Pack_edit />} />
+          <Route path="/admin/packs/:id" element={<PackShow />} />
+          <Route path="/admin/packs/create" element={<Pack_create />} />
+          <Route path="/admin/packs/edit/:id" element={<Pack_edit />} />
           
-        <Route path="/dashboardTienda" element={<DashboardTienda />} />
-          <Route path="/store/category/:id" element={<CategoryPage />} />
+          <Route path="/" element={<DashboardTienda />} />
+          <Route path="/category/:id" element={<CategoryPage />} />
 
         </Routes>
 
