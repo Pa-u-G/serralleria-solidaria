099ffd2332bff72324cdab62c72a3c2885b1723a
hatimi14 <hatim.kenfaui@inslapineda.cat>
2026-04-09 22:00:03 +0200

    scroll

--- frontend/src/pages/store/dashboardTienda/dashboard.jsx
+++ frontend/src/pages/store/dashboardTienda/dashboard.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 //import ProductGrid from '../components/productGrid/ProductGrid';
 import Carousel from '../components/carousel/Carousel';
+import styles from './dashboard.module.scss';
 
 function Dashboard() {
   const [products, setProducts] = useState([]);
@@ -12,10 +13,20 @@ function Dashboard() {
       .then(res => setProducts(res.data))
       .catch(err => console.log(err));
   }, []);
-
+  const featuredProducts = products.filter(product => product.star === 1 && product.status == true);
+  const longitud = featuredProducts.length;
+  console.log(longitud);
   return(
     <MainLayout>
-      <Carousel products={products} limit={10} />
+      <h1>Productos destacados</h1>
+      
+      {featuredProducts.length > 0 ? (
+        <Carousel products={featuredProducts} limit={10} />
+      ) : (
+        <div className={styles.emptyState}>
+          <p>Actualmente no hay productos destacados</p>
+        </div>
+      )}
       
     </MainLayout>
   )
